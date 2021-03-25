11ec79ed9f2d77656aee271f48fa8870bb3ff966
Bonita Scott <bonitalscott@yahoo.com>
2021-03-25 19:15:32 -0400

    Challenge04 Covert from Functional to Class Component w/o Bind

--- red-badge-demo-ts/src/components/ClassClickCounter.jsx
+++ red-badge-demo-ts/src/components/ClassClickCounter.jsx
@@ -1,1 +1,23 @@
+import React, { Component } from 'react';
+import { Button, Container } from 'reactstrap';
+
+class ClassClickCounter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { countValue: this.props.initialValue };
+  }
+  handleClick = () => {
+    this.setState({ countValue: this.state.countValue -1 });
+  };
+  render() {
+    return (
+      <Container>
+        <p>The total clicks left are {this.state.countValue}</p>
+        <Button onClick={this.handleClick}> Click Me from ClassClick </Button>
+      </Container>
+    );
+  }
+}
+export default ClassClickCounter;
+
 // Create a class version of the Functional Component
